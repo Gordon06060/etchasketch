bf730f476cc5313cf85b5822462a10ac559c04fa
Gordon06060 <gordonz0913@gmail.com>
2024-05-01 17:33:54 -0700

    Added a button that prompts the user for the number of grids, deletes the old box and creates a new grid with whatever number of squares that the user has requested

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const container = document.querySelector('#container');
+const newContainer = document.querySelector('#newContainer');
 
 function changeColor(event){
     event.target.style.backgroundColor = 'red';
@@ -7,6 +8,8 @@ function changeColor(event){
 function changeColorBack(event){
     event.target.style.backgroundColor = 'white';
 }
+
+
 
 for (n=0; n<16; n++){
     const rowdiv = document.createElement('div');
@@ -22,25 +25,26 @@ for (n=0; n<16; n++){
     };
 };
 
-// function createGrid(){
-//     let input = prompt('How many grids do you want');
-//     for (n=0; n<input; n++){
-//         const rowdiv = document.createElement('div');
-//         rowdiv.classList.add('rowdiv');
-//         container.appendChild(rowdiv);
-//         for (i=0; i<input; i++){
-//             const div1 = document.createElement('div');
-//             div1.classList.add('div1');
-//             rowdiv.appendChild(div1);
-//             div1.addEventListener('mouseover', changeColor);
-//             div1.addEventListener('mouseout', changeColorBack);
-    
-//         };
-//     };
-// };
+function createGrid(){
+    let input = prompt('How many grids do you want');
+    container.remove();
+    for (n=0; n<input; n++){
+        const rowdiv = document.createElement('div');
+        rowdiv.classList.add('rowdiv');
+        newContainer.appendChild(rowdiv);
+        for (i=0; i<input; i++){
+            const div1 = document.createElement('div');
+            div1.classList.add('div1');
+            rowdiv.appendChild(div1);
+            div1.addEventListener('mouseover', changeColor);
+            div1.addEventListener('mouseout', changeColorBack);
 
-// const button = document.querySelector('#gridbutton');
-// button.addEventListener('click', createGrid);
+        };
+    };
+};
+
+const button = document.querySelector('#gridbutton');
+button.addEventListener('click', createGrid);
 
 
 
